867933c7348f14a20109f30a7474cf6872b86010
gigicupp <gigicupp@gmail.com>
2021-01-14 20:43:17 -0800

    products table has 100 fake records

--- database/index.js
+++ database/index.js
@@ -9,3 +9,16 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
+const insertIntoProducts = (dataSet) => {
+  return dataSet.forEach(data => {
+    return connection.query(`INSERT INTO products (prod_name, prod_desc) VALUES ("${data[0]}", "${data[1]}")`, (err, result) => {
+      if(err) {
+        console.log(err, 'Error inserting into database')
+      } else {
+        console.log('success inserting into database');
+      }
+    });
+  })
+}
+
+module.exports.insertIntoProducts = insertIntoProducts;
