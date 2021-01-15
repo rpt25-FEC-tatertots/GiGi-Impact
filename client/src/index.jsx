06dfbc18877f3e46cd0f39da64f0061429e31b7b
gigicupp <gigicupp@gmail.com>
2021-01-14 19:47:29 -0800

    added ! to test

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Hello World!</h1>
         <Materials />
         <Locations />
       </div>
