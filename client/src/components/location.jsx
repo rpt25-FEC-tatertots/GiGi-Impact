937550ef2f5eba63b6d5dc6415e5a81b91ce0506
gigicupp <gigicupp@gmail.com>
2021-01-24 21:17:37 -0800

    styled component for slider buttons

--- client/src/components/location.jsx
+++ client/src/components/location.jsx
@@ -3,46 +3,25 @@ import React from 'react';
 class Location extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      index: 0
-    }
-    this.slideRight = this.slideRight.bind(this);
-    this.slideLeft = this.slideLeft.bind(this);
-  }
 
-  slideRight(event) {
-    event.preventDefault();
-    this.setState(prevState => {
-      return {
-        index: ((prevState.index + 1) % this.props.locations.length)
-      }
-    })
-  }
-
-  slideLeft(event) {
-    event.preventDefault();
-    if (this.state.index === 0) {
-      this.setState({
-        index: this.props.locations.length - 1
-      })
-    } else {
-      this.setState(prevState => {
-        return {
-          index: prevState.index - 1
-        }
-      })
-    }
   }
 
   render() {
     let locations = this.props.locations;
+    let pics = locations.map(location => {
+      const { loc_pic, loc_company, loc_name, id } = location;
+      return (
+        <div className='card' key={id}>
+          <img src={loc_pic}/>
+          <div>{loc_name}</div>
+        </div>
+      )
+    })
     console.log(locations);
     return (
       locations.length > 0 && (
         <div>
-          <button onClick={this.slideLeft}>{'<'}</button>
-          <img src={locations[this.state.index].loc_pic}/>
-          <button onClick={this.slideRight}>{'>'}</button>
+          {pics}
         </div>
       )
     )
--- client/src/components/locations.jsx
+++ client/src/components/locations.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import axios from 'axios';
 import Location from './location.jsx';
+import styled from 'styled-components';
 
+const SliderButton = styled.button`
+  border-radius: 50%;
+  border: none;
+  background-color: #fff;
+  height: 4rem;
+  width: 4rem;
+  &:hover{
+    box-shadow: 0 8px 20px 0 rgba(136, 18, 18, 0.2);
+  };
+`
 
 class Locations extends React.Component {
   constructor() {
     super();
     this.state = {
-      locations: []
+      locations: [],
+      index: 0
     }
+    this.slideRight = this.slideRight.bind(this);
+    this.slideLeft = this.slideLeft.bind(this);
+
     this.getProductById = this.getProductById.bind(this);
+
   }
 
   getProductById() {
     let randIndex = Math.floor(Math.random() * 100) + 1;
     axios.get(`/api/locations?product_id=${randIndex}`)
       .then(response => this.setState({ locations: response.data }))
-      // .then(response => console.log('data on client 🇹🇭', response.data))
       .catch(error => console.log(error))
   }
 
@@ -24,11 +39,45 @@ class Locations extends React.Component {
     this.getProductById();
   }
 
+  slideRight(event) {
+    event.preventDefault();
+    this.setState(prevState => {
+      return {
+        index: ((prevState.index + 1) % this.state.locations.length)
+      }
+    })
+  }
+
+  slideLeft(event) {
+    event.preventDefault();
+    if (this.state.index === 0) {
+      this.setState({
+        index: this.state.locations.length - 1
+      })
+    } else {
+      this.setState(prevState => {
+        return {
+          index: prevState.index - 1
+        }
+      })
+    }
+  }
+
   render() {
     return (
       <div>
         <button>Where it is made</button>
-        <Location locations={this.state.locations}/>
+        <SliderButton onClick={this.slideLeft}>
+          <span className="material-icons">
+            keyboard_arrow_left
+          </span>
+        </SliderButton>
+        <Location locations={this.state.locations} />
+        <SliderButton onClick={this.slideRight}>
+          <span className="material-icons">
+            keyboard_arrow_right
+          </span>
+        </SliderButton>
       </div>
     )
   }
