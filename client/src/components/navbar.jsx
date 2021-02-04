da5027350cdf4111e0e9f35663e87b71bd9fe1ef
gigicupp <gigicupp@gmail.com>
2021-02-04 11:34:31 -0800

    slider bar under the tabs work

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -3,7 +3,7 @@ import Styled from 'styled-components';
 
 const NavDiv = Styled.div`
   position: relative;
-  background: olive;
+  background: #fff;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -22,16 +22,20 @@ const NavItems = Styled.ul`
   padding: 0;
 `
 const Marker = Styled.div`
-  position: absolute;
+  position: relative;
   height: 4px;
   width: 100%;
   background: #000;
-  bottom: 2px;
+  /* bottom: 2px; */
   transition: 0.5s;
   border-radius: 4px;
+  opacity: 0.2;
   /* left: 4px; */
 `
-const Nav = Styled.div`
+const MarkerSelected = Styled(Marker)`
+  opacity: 1;
+`
+const Nav = Styled.li`
   padding: 0;
   border: 0;
   font-size: 100%;
@@ -46,10 +50,13 @@ const Nav = Styled.div`
 const Box = Styled.div`
   position: relative;
   display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
   justify-content: center;
   align-items: center;
   margin: 0;
   padding: 0;
+  background-color: transparent;
 `
 
 class Navbar extends React.Component {
@@ -57,45 +64,42 @@ class Navbar extends React.Component {
     super()
 
     this.state = {
-      currentTab: '',
-      left: 0,
-      width: 0,
+      currentTab: 'materials',
+      isSelected: true
     }
+
     this.myRef = React.createRef();
     this.handlePick = this.handlePick.bind(this);
-    this.getOffset = this.getOffset.bind(this)
   }
 
-  
+
   handlePick(event) {
     event.preventDefault();
-    this.setState({ currentTab: event.target.id })
+    console.log(event.target.id)
+    this.setState(
+      { currentTab: event.target.id,
+        isSelected: true
+      }
+    )
   }
-
-  getOffset(event) {
-    // this.setState({ 
-    //   left: this.myRef.current.offsetLeft,
-    //   width: this.myRef.current.offsetWidth
-    // }) 
-    console.log(this.myRef.current.offsetLeft + 'px');
-    console.log(this.myRef.current.offsetWidth + 'px');
-  }
-
-  
 
   render() {
+    const {currentTab, isSelected} = this.state
     return <NavDiv>
-      <Box>
         <NavItems >
-          <Nav onClick={this.handlePick} onClick={this.getOffset} ref={this.myRef} id='materials' >
-            How it's made
-          </Nav>
-          <Nav onClick={this.handlePick} onClick={this.getOffset} ref={this.myRef} id='locations' >
-            Where it's made
-          </Nav>
+          <Box>
+            <Nav onClick={this.handlePick} ref={this.myRef} id='materials' >
+              How it's made
+            </Nav>
+            {currentTab === 'materials' && isSelected ? <MarkerSelected></MarkerSelected> : <Marker></Marker>}
+          </Box>
+          <Box>
+            <Nav onClick={this.handlePick} ref={this.myRef} id='locations' >
+              Where it's made
+            </Nav>
+            {currentTab === 'locations' && isSelected ? <MarkerSelected></MarkerSelected> : <Marker></Marker>}
+          </Box>
         </NavItems>
-        <Marker></Marker>
-      </Box>
     </NavDiv>
   }
 }
