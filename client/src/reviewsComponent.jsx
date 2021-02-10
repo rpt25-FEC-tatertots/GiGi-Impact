a58d1ca4573b27213330a08af8b0f3814b60cc77
Mason Pott <masoncpott@gmail.com>
2021-02-10 14:29:17 -0600

    created Reviews Component, rendered to DOM

---
+++ client/src/reviewsComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ReviewsComponent extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>HELLO WORLD</div>
+    )
+  }
+}
+
+
+export default ReviewsComponent;
